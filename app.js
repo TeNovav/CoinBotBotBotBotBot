14c5c61b656f45d4f0d9afb8b955aa7b9d164103
TeNovav <artpro1324@gmail.com>
2024-08-03 16:22:55 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,22 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
         return localStorage.getItem('user_id') || 'default_user_id';
     };
 
-    // Пример установки начального баланса
-    userBalance.textContent = '0.05 BTC';
 
     // Обработчик кнопки "Майнить"
     mineBtn.addEventListener('click', () => {
         const userId = getUserId();
-        fetch(http://127.0.0.1:8000/mine?user_id=${userId})
+        fetch('http://127.0.0.1:8000/mine_block')
             .then(response => response.text())
             .then(data => {
-                alert(data);  // Показываем ответ от сервера пользователю
-                // Обновляем баланс пользователя в интерфейсе
-                fetch(http://127.0.0.1:8000/get_balance?user_id=${userId})
-                    .then(response => response.text())
-                    .then(balanceData => {
-                        userBalance.textContent = balanceData.split(' ')[2] + ' BTC';
-                    });
+                alert(data);
             })
             .catch(error => {
                 console.error('Error:', error);
